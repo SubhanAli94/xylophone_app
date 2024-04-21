23d075af8d73df914e32c21a02fb9f0846ad7528
Subhan Ali <subhan@1001dubai.com>
2024-04-21 06:44:56 +0500

    Audio Player lib added.

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: TextButton(
+            onPressed: () {
+              final player = AudioCache();
+              player.play('note1.wav');
+            },
+            child: Text('Click Me'),
+          ),
+        ),
+      ),
+    );
+  }
+}
